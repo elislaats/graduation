de47b0886afc38ab219c6f52976db2887547e2ae
Eli <eli.slaats@student.fontys.nl>
2023-04-04 12:58:43 +0200

    added template with cre8ion template & styles

---
+++ vue-template-Eli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+
+export default {
+  setup() {
+    const store = useStore();
+    const isAuthenticated = computed(() => store.getters.isAuthenticated);
+
+    store.dispatch('autoLogin');
+
+    return { isAuthenticated };
+  }
+};
+</script>
---
+++ vue-template-Eli/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid">
+    <div class="col-1-1 flex justify-end">
+      <button
+        @click="store.dispatch('logout')"
+        class="btn btn-primary text-uppercase rounded-corners"
+      >
+        Uitloggen
+      </button>
+    </div>
+  </div>
+  <div class="grid">
+    <div class="col-1-1">
+      <h2>Home screen</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+export default {
+  name: 'HomeScreen',
+  setup() {
+    return { store: useStore() };
+  }
+};
+</script>
