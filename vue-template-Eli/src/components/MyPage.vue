fe3faae320703fbfec9119e279cf4b66d197bce1
Eli <eli.slaats@student.fontys.nl>
2023-04-04 16:08:08 +0200

    added function to get page data

--- vue-template-Eli/src/components/MyPage.vue
+++ vue-template-Eli/src/components/MyPage.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     props: {
         id: {
@@ -10,8 +12,20 @@ export default {
             required: true
         },
     },
-    async created(){
-        
+    async created() {
+        await this.getPageData();
     },
+    methods: {
+        async getPageData() {
+            const apiUrl = `https://api-cre8ion.tc8l.dev/api/page/` + this.id
+            try {
+                await axios.get(apiUrl).then(response => {
+                    console.log(response)
+                })
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
 };
 </script>
