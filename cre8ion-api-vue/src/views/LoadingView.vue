5ce82833d4ae7ab17e1faee48bd992e2433a0dd4
Eli <eli.slaats@student.fontys.nl>
2023-04-11 10:23:30 +0200

    refactor & clean up template

---
+++ cre8ion-api-vue/src/views/LoadingView.vue
@@ -0,0 +1,11 @@
+<script setup>
+const message = "Loading..."
+</script>
+
+<template>
+  <div class="grid">
+    <div class="col-1-1">
+      <h2>{{ message }}</h2>
+    </div>
+  </div>
+</template>
