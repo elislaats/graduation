027d09b59a3aa1ad7a68f6dd8acf9122a1f3113f
elislaats <71012148+elislaats@users.noreply.github.com>
2023-04-13 16:36:02 +0200

    added loading spinner

--- cre8ion-api-vue/src/views/LoadingView.vue
+++ cre8ion-api-vue/src/views/LoadingView.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="grid">
-    <h4 class="col-1-1 text-danger">
-      Loading...
-    </h4>
-  </div>
+  <main class="flex align-center justify-center">
+    <div class="load-spinner" />
+  </main>
 </template>
