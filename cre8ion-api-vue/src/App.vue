cfbcd5fcfcb6fb659ee7b5754e96456d906fc5e5
Eli <eli.slaats@student.fontys.nl>
2023-04-19 11:52:03 +0200

    show github link only on production

--- cre8ion-api-vue/src/App.vue
+++ cre8ion-api-vue/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { ref } from "vue";
 import NavBar from "./components/NavBar.vue";
 import GitHubLink from "./components/GitHubLink.vue";
+
+const environment = ref(process.env.NODE_ENV)
 </script>
 
 <template>
   <NavBar />
-  <GitHubLink />
+  <GitHubLink v-if="environment != 'development'"/>
   <router-view />
 </template>
