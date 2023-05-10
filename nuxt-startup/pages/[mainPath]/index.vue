64fa734c65b5c83e36829693d4afcd6e3de99799
Eli <eli.slaats@student.fontys.nl>
2023-05-10 15:09:46 +0200

    added getting ID

--- nuxt-startup/pages/[mainPath]/index.vue
+++ nuxt-startup/pages/[mainPath]/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <h3>Main route</h3>
-  <p>{{ $route.params }}</p>
+  <main>
+    <h3>Main route</h3>
+    <p>{{ $route.params }}</p>
+    <p>id: {{ currentId }}</p>
+  </main>
 </template>
 
 <script setup>
 definePageMeta({
   middleware: ["check-main-path"],
 });
+
+function getContentId() {
+  const currentRoute = useRoute();
+  const routes = useState("routes").value;
+  routes.forEach((route) => {
+    if (route.path == currentRoute.fullPath) {
+      currentId.value = route.id;
+    }
+  });
+}
+
+const currentId = ref();
+
+onMounted(() => getContentId());
 </script>
