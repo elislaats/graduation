b4c4abdfb9f8b2ab2cad62b90c5a47f1b260bea2
Eli <eli.slaats@student.fontys.nl>
2023-05-23 14:52:43 +0200

    add image component

---
+++ nuxt-startup/components/contentblocks/AfbeeldingBlock.vue
@@ -0,0 +1,50 @@
+<script setup>
+const props = defineProps({
+  info: {
+    type: Object,
+    required: true,
+  },
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <section
+    class="contentblock"
+    :class="info._name.toLowerCase().replaceAll(' ', '-')"
+    v-if="info && data"
+  >
+    <div class="grid grid-pad">
+      <div class="img-container col-1-1">
+        <template v-if="data.link">
+          <NuxtLink :to="data.link">
+            <ImageComp
+              v-if="data.afbeelding"
+              :id="data.afbeelding"
+              :width="1200"
+              :className="'link-img'"
+            ></ImageComp>
+          </NuxtLink>
+        </template>
+      </div>
+    </div>
+  </section>
+  <section class="contentblock text-danger" v-else>
+    <p>no content available</p>
+  </section>
+</template>
+
+<style lang="scss">
+.contentblock.afbeelding {
+  img {
+    display: block;
+    width: 100%;
+    max-width: 100%;
+    margin: 0;
+    margin-bottom: 2rem;
+  }
+}
+</style>
