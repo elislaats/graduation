72a33b15997f2b4d06dd252bf42f4c2d16378c76
Eli <eli.slaats@student.fontys.nl>
2023-05-23 16:17:19 +0200

    add video component + fix header sizing

--- nuxt-startup/components/contentblocks/HeaderBlock.vue
+++ nuxt-startup/components/contentblocks/HeaderBlock.vue
@@ -17,19 +17,23 @@ const props = defineProps({
     :class="info._name.toLowerCase().replaceAll(' ', '-')"
     v-if="info && data"
   >
-    <div class="inner grid no-p align-content-center">
+    <!--background video-->
+    <div class="video-wrapper">
+      <VideoComp
+        :url="data.videoUrl"
+        :fallback="data.afbeelding"
+        :className="'header-bg'"
+      >
+      </VideoComp>
+    </div>
+
+    <!--content: -->
+    <div class="header-inner grid no-p align-content-center">
       <div class="header-text col-8-12">
         <h1 v-if="data.titel" v-html="data.titel" />
         <div v-if="data.tekst" v-html="data.tekst" />
       </div>
     </div>
-    <!--background-image-->
-    <ImageComp
-      v-if="data.afbeelding"
-      :id="data.afbeelding"
-      :width="1200"
-      :className="'bg-image header-bg'"
-    ></ImageComp>
   </section>
   <section class="contentblock text-danger" v-else>
     <p>no content available</p>
@@ -37,11 +41,26 @@ const props = defineProps({
 </template>
 
 <style lang="scss">
-.header{
-  .header-text{
-    z-index: 4;
+.contentblock.header {
+  position: relative;
+  min-height: 100vh;
+  margin-bottom: 10rem;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .header-inner {
+    width: 100%;
+    position: relative;
+    z-index: 3;
+    padding: 20rem 0;
+    .header-text {
+      z-index: 4;
+    }
   }
-  .header-bg{
+  .video-wrapper {
     position: absolute;
     top: 0;
     left: 0;
@@ -49,6 +68,21 @@ const props = defineProps({
     right: 0;
     width: 100%;
     height: 100%;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+    .header-bg {
+      box-sizing: border-box;
+      height: 56.25vw;
+      left: 50%;
+      min-height: 100%;
+      min-width: 100%;
+      transform: translate(-50%, -50%) scale(1.2);
+      position: absolute;
+      top: 50%;
+      width: 177.77777778vh;
+    }
   }
 }
 </style>
--- nuxt-startup/components/contentblocks/VideoBlock.vue
+++ nuxt-startup/components/contentblocks/VideoBlock.vue
@@ -13,17 +13,36 @@ const props = defineProps({
 
 <template>
   <section
-    class="contentblock" :class="info._name.toLowerCase().replaceAll(' ', '-')"
-    v-if="props.info && props.data"
+    class="contentblock"
+    :class="info._name.toLowerCase().replaceAll(' ', '-')"
+    v-if="info && data"
   >
-    <h2 class="col-1-1" v-if="props.data.titel">
-      {{ props.data.titel }}
-    </h2>
-    <div class="col-1-1 border-white text-primary">
-      video with url {{ props.data.linkUrl }}
+    <div class="grid grid-pad">
+      <h2 class="col-1-1" v-if="data.titel">
+        {{ props.data.titel }}
+      </h2>
+      <div class="video-wrapper col-1-1">
+        <VideoComp :url="data.linkUrl"> </VideoComp>
+      </div>
     </div>
   </section>
   <section class="contentblock text-danger" v-else>
     <p>no content available</p>
   </section>
 </template>
+
+<style lang="scss">
+.video-wrapper {
+  position: relative;
+  padding-bottom: 56.25%;
+  width: 100%;
+  height: 0;
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
