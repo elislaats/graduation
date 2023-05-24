a5c095a615c7b92faae0a0379b8b9c3a010230bf
Eli <eli.slaats@student.fontys.nl>
2023-05-24 13:55:07 +0200

    move page loading wheel to component

---
+++ nuxt-startup/components/LoadingComp.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="loading-indicator-wrapper" :class="props.color">
+    <div class="loading-indicator"></div>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<style lang="scss">
+.loading-indicator-wrapper {
+  z-index: 0;
+  height: 100vh;
+  width: 100vw;
+  padding: 0;
+  margin: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .loading-indicator {
+    width: 7rem;
+    height: 7rem;
+    border-radius: 100%;
+    border: 1rem dotted $primary-color;
+    animation: spinning 3s linear infinite;
+  }
+  &.white {
+    div {
+      border-color: $white;
+    }
+  }
+}
+
+@keyframes spinning {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- nuxt-startup/pages/[mainPath]/index.vue
+++ nuxt-startup/pages/[mainPath]/index.vue
@@ -8,9 +8,7 @@
       :data="block.data"
     />
   </main>
-  <section class="loading-indicator" v-else>
-    <div></div>
-  </section>
+  <LoadingComp v-else></LoadingComp>
 </template>
 
 <script setup>
@@ -66,35 +64,3 @@ onMounted(async () => {
   }
 });
 </script>
-
-<style lang="scss">
-.loading-indicator {
-  z-index: 0;
-  height: 100vh;
-  width: 100vw;
-  padding: 0;
-  margin: 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  div {
-    width: 7rem;
-    height: 7rem;
-    border-radius: 100%;
-    border: 1rem dotted $primary-color;
-    animation: spinning 3s linear infinite;
-  }
-}
-
-@keyframes spinning {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-</style>
