caf9ef3e4cbfa7e062df1cfa2756f000099a8649
Eli <eli.slaats@student.fontys.nl>
2023-05-24 15:42:34 +0200

    add error component

---
+++ nuxt-startup/components/ErrorComp.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  error: {
+    type: Object,
+    required: false,
+  },
+});
+</script>
+<template>
+  <section class="flex align-center justify-center">
+    <div class="text-danger">
+      <h3>Sorry, er ging iets mis</h3>
+      <p v-if="error">
+        {{ error }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  width: 100%;
+  height: 100vh;
+  h3 {
+    font-size: large;
+  }
+}
+</style>
