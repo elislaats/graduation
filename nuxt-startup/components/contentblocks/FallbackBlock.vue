3af3d36a54c67945a9c8ada439bbf41d3a03087a
Eli <eli.slaats@student.fontys.nl>
2023-05-25 13:40:53 +0200

    new style for fallback

--- nuxt-startup/components/contentblocks/FallbackBlock.vue
+++ nuxt-startup/components/contentblocks/FallbackBlock.vue
@@ -12,14 +12,21 @@ const props = defineProps({
 </script>
 
 <template>
-  <section class="contentblock" v-if="props.info && props.data">
-    <h4>
-      Geen component gevonden voor
-      <em class="text-primary">
-        {{ props.info._id }}: {{ props.info._name }}</em
-      >
-    </h4>
-    <p><strong class="text-primary"> Data: </strong>{{ props.data }}</p>
+  <section class="contentblock" v-if="info && data">
+    <div class="grid grid-pad bg-grey-dark p-2">
+      <div class="col-1-3">
+        <h4 class="text-primary">{{ info._id }}: {{ info._name }}</h4>
+        <h5>Geen component gevonden</h5>
+      </div>
+      <div class="col-2-3">
+        <p :style="{ fontSize: 'larger' }" class="mb-1"><strong>Data:</strong></p>
+        <p v-for="(value, key) in data" class="mb-1">
+          <span class="text-primary">{{ key }}: </span>
+          <span v-if="value">{{ value }}</span>
+          <i class="text-danger" v-else>leeg</i>
+        </p>
+      </div>
+    </div>
   </section>
   <section class="contentblock text-danger" v-else>
     <p>no content available</p>
