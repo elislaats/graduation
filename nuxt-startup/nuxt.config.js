70218d881abac7074ea7fa2aa56fb0be1cfa61a2
Eli <eli.slaats@student.fontys.nl>
2023-05-26 10:59:28 +0200

    add nitro config hook

--- nuxt-startup/nuxt.config.js
+++ nuxt-startup/nuxt.config.js
@@ -1,16 +1,27 @@
 export default defineNuxtConfig({
   target: "static",
-  ssr: false,
+  ssr: true,
   pages: true,
   vite: {
     css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@import "~/assets/styles/main.scss";',
-            },
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~/assets/styles/main.scss";',
         },
+      },
     },
-},
+  },
+  hooks: {
+    async "nitro:config"(nitroConfig) {
+      const response = await fetch(
+        "https://api-cre8ion.tc8l.dev/api/navigation"
+      );
+      const routes = await response.json();
+      routes.forEach((route) => {
+        nitroConfig.prerender.routes.push(route.url);
+      });
+    },
+  },
   devtools: {
     enabled: true,
   },
