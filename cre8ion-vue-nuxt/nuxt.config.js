ece89d0e22c6a4fbe954ea831f950cf402548f03
Eli <eli.slaats@student.fontys.nl>
2023-05-30 14:02:17 +0200

    load routes in extend:pages hook

--- cre8ion-vue-nuxt/nuxt.config.js
+++ cre8ion-vue-nuxt/nuxt.config.js
@@ -6,14 +6,41 @@ export default defineNuxtConfig({
       apiBase: "https://api-cre8ion.tc8l.dev/api/",
     },
   },
-  css: ['~/assets/styles/main.scss'],
+  css: ["~/assets/styles/main.scss"],
   hooks: {
     async "pages:extend"(pages) {
-      pages.push({
-        name: "homepage",
-        path: "/homepage",
-        file: "~/pages/index.vue",
+      const response = await fetch(
+        "https://api-cre8ion.tc8l.dev/api/navigation"
+      );
+      const routes = await response.json();
+      routes.forEach((route, index) => {
+        if (index == 0) {
+          setHomepage(route);
+        } else {
+          addPage(route.name, route.url, route.id);
+        }
       });
+
+      function addPage(name, path, id) {
+        pages.push({
+          name: name,
+          path: path,
+          file: "~/pages/index.vue",
+          props: { id: id },
+          children: [
+            {
+              path: ":slug",
+              file: "~/pages/index.vue",
+            },
+          ],
+        });
+      }
+
+      function setHomepage(route) {
+        pages[0].name = route.name;
+        pages[0].alias = route.url;
+        pages[0].props = { id: route.id };
+      }
     },
   },
   devtools: {
