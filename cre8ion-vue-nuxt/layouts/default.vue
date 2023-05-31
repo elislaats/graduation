9ed3c27906f1ea04a5af92efd31d3d7118e179e6
Eli <eli.slaats@student.fontys.nl>
2023-05-31 11:25:41 +0200

    add loading indicator

--- cre8ion-vue-nuxt/layouts/default.vue
+++ cre8ion-vue-nuxt/layouts/default.vue
@@ -1,4 +1,16 @@
+<script setup>
+const nuxtApp = useNuxtApp();
+const loading = ref(false);
+nuxtApp.hook("page:start", () => {
+  loading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+  loading.value = false;
+});
+</script>
+
 <template>
   <NavBar></NavBar>
+  <LoadingComp v-if="loading"></LoadingComp>
   <slot />
 </template>
