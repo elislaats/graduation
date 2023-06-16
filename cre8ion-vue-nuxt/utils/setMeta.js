275fd029f039e44762ca0ea1568a39e8a0f745ac
Eli <eli.slaats@student.fontys.nl>
2023-06-16 15:43:18 +0200

    add extra check on availability of metadata

--- cre8ion-vue-nuxt/utils/setMeta.js
+++ cre8ion-vue-nuxt/utils/setMeta.js
@@ -1,6 +1,6 @@
 export default function (page) {
   const meta = page.metaData;
-  if (meta) {
+  if (meta && meta['title']) {
     useSeoMeta(meta);
   } else {
     useSeoMeta({ title: page.info.titel + " | The Cre8ion.Lab" });
